e73ea64f28d5fa85e23319ed99517aa7195ba4ed
iiyuu16 <pcg4m3r16@gmail.com>
2023-11-27 23:15:52 +0800

    minor cleaning
    add music

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.UIElements.Experimental;
 
 [System.Serializable]
 public class Dialogue 
