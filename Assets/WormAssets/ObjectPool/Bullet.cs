7f1c1fa6972e5af51aa01574518791045c149826
ShadowIWS <rocheandre123@gmail.com>
2024-08-09 13:26:57 +0800

    more worm additions

---
+++ Assets/WormAssets/ObjectPool/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : AutoDestroyPoolableObject
+{
+    [HideInInspector]
+    public Rigidbody rigidbody;
+    public Vector3 speed = new Vector3(200, 0);
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        rigidbody.velocity = speed;
+
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        rigidbody.velocity = Vector3.zero;
+    }
+}
--- Assets/WormAssets/wormEnemyMovement.cs
+++ Assets/WormAssets/wormEnemyMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class wormEnemyMovement : MonoBehaviour
 {
@@ -11,26 +12,67 @@ public class wormEnemyMovement : MonoBehaviour
 
     private GameObject[] WPs;
     private NavMeshAgent agent;
+    private int arrived = 1;
+    private int chase = 0;
     void Start()
     {
         WPs = GameObject.FindGameObjectsWithTag("Waypoint");
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(FollowTarget());
+        if (arrived == 1 && chase == 0)
+        {
+            target = WPs[Random.Range(0, WPs.Length)].transform;
+            arrived = 0;
+            StartCoroutine(FollowTarget());
+            
+        }
+        else
+        {
+            //Debug.Log("EMPTY");
+        }
+
+        if (this.transform.position.x == target.transform.position.x && this.transform.position.z == target.transform.position.z)
+        {
+            arrived = 1;
+            Debug.Log("ARRIVED");
+        }
     }
 
     private IEnumerator FollowTarget()
     {
         WaitForSeconds wait = new WaitForSeconds(speed);
-        target = WPs[Random.Range(0, WPs.Length)];
-        while (enabled)
+        agent.SetDestination(target.transform.position);
+        Debug.Log(target.gameObject.name);
+        while (this.transform.position != target.transform.position)
         {
-            agent.SetDestination(target.transform.position);
             yield return wait;
+
         }
         
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("PLAYER DETECTED");
+            chase = 1;
+            target = other.transform;
+            StartCoroutine(FollowTarget());
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        chase = 0;
+        Debug.Log("Chase over");
     }
 }
