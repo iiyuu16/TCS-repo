b4be7f9c7b28c42e75690560bc24e74009b6df29
ShadowIWS <rocheandre123@gmail.com>
2024-09-17 19:15:55 +0800

    worm model added, camera shake and hp added

--- Assets/WormAssets/playerHP.cs
+++ Assets/WormAssets/playerHP.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerHP : MonoBehaviour
 {
     public BoxCollider collide;
-    public int HP = 10;
+    public int HP;
+    public GameObject healthbar;
+    public Slider HealthBar;
+
+    public Camera Camera;
+    private CameraShake camshake;
+    
+
+
 
     private int playerHealth = 10;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //HealthBar = GetComponent<Slider>();
         collide = this.GetComponent<BoxCollider>();
+        HealthBar.maxValue = HP;
+
+        camshake = Camera.GetComponent<CameraShake>();
     }
 
     // Update is called once per frame
@@ -26,10 +40,14 @@ public class playerHP : MonoBehaviour
         if(other.gameObject.tag == "Bullet")
         {
             HP--;
+            camshake.shakeDuration = 0.7f;
             if (HP < 1)
             {
                 this.gameObject.SetActive(false);
             }
+            
+            HealthBar.value = HP;
+            Debug.Log(HealthBar.value);
         }
     }
 }
